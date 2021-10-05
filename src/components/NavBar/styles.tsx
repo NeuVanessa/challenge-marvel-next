cb075e0445c38250d97e58a64a27d8c5d0fbc726
VanessaLops <vanessashirlenee_estrela@hotmail.com>
2021-10-05 01:57:18 -0300

    conflit prettier sorted out

--- src/components/NavBar/styles.tsx
+++ src/components/NavBar/styles.tsx
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
-import Button from '@mui/material/Button';
 
 export const HeaderContainer = styled.div`
   display: flex;
-  background: #1F3042;
+  background: #1f3042;
   height: 50px;
   justify-content: space-between;
   align-items: center;
@@ -12,7 +11,6 @@ export const HeaderContainer = styled.div`
     color: white;
     text-decoration: none;
     align-self: center;
-    font-family: sans-serif,
+    font-family: sans-serif;
   }
 `;
-
